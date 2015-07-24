cc050ebc9a2704f276ab64b284a0532b2068408e
Philip Necsulescu <necsup@gmail.com>
2015-07-24 11:38:24 +0200

    call say_hello(name) from view and print string

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+require 'csv'
 
     def campaigns
         if params[:campaign_id].to_i >= 5
@@ -6,7 +7,15 @@ class PagesController < ApplicationController
             csv_convs = File.read("app/assets/csv/conversions_1.csv")
             csv_imps = File.read("app/assets/csv/impressions_1.csv")
 
-            csv_clicks= CSV.parse(csv_clicks, :headers => true)
+            csv_clicks = CSV.parse(csv_clicks, :headers => true)
+            csv_convs =  CSV.parse(csv_convs, :headers => true)
+            csv_imps = CSV.parse(csv_imps, :headers => true)
         end
+    end
+
+    def say_hello(name)
+        hello_test = "hello, " + name
+        hello_test += " welcome"
+        return hello_test
     end
 end
