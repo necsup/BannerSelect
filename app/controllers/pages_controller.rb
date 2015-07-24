dece8779aa6db8ee62f00d4a96d6e0be53033292
Philip Necsulescu <necsup@gmail.com>
2015-07-24 06:54:29 +0200

    CSV files loaded

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,11 @@ class PagesController < ApplicationController
 
     def campaigns
         if params[:campaign_id].to_i >= 5
-            csv_text = File.read('blah')
+            csv_clicks= File.read("app/assets/csv/clicks_1.csv")
+            csv_convs = File.read("app/assets/csv/conversions_1.csv")
+            csv_imps = File.read("app/assets/csv/impressions_1.csv")
+
+            csv_clicks= CSV.parse(csv_clicks, :headers => true)
         end
     end
 end
