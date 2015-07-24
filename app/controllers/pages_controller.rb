36e292b54755524ee35ffe22c99ad33fa7622b3a
Philip Necsulescu <necsup@gmail.com>
2015-07-24 16:16:04 +0200

    banner_revenue subset without summing finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,32 +9,51 @@ class PagesController < ApplicationController
     
       
       csv_clicks= CSV.read("app/assets/csv/clicks_1.csv", {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all} )
+      csv_conversions= CSV.read("app/assets/csv/conversions_1.csv", {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all} )
         hello_test = campaign_id.to_s +  "\n"
 
-        hashed_data = csv_clicks.map { |d| d.to_hash }
-        hello_test += hashed_data[0][:click_id].to_s + "\n"
-        hello_test += hashed_data[1][:click_id].to_s + "\n"
-        hello_test += hashed_data[2][:click_id].to_s + "\n"
+        clicks_data = csv_clicks.map { |d| d.to_hash }
+        conversions_data = csv_conversions.map { |d| d.to_hash}
+        hello_test += clicks_data[0][:click_id].to_s + "\n"
+        hello_test += clicks_data[1][:click_id].to_s + "\n"
+        hello_test += clicks_data[2][:click_id].to_s + "\n"
 
-        hashed_data.reverse!
-        hello_test += hashed_data[0][:click_id].to_s + "\n"
-        hello_test += hashed_data[1][:click_id].to_s + "\n"
-        hello_test += hashed_data[2][:click_id].to_s + "\n"
+        clicks_data.reverse!
+        hello_test += clicks_data[0][:click_id].to_s + "\n"
+        hello_test += clicks_data[1][:click_id].to_s + "\n"
+        hello_test += clicks_data[2][:click_id].to_s + "\n"
 
-        hashed_data.sort_by! { |v| v[:banner_id]}
+        clicks_data.sort_by! { |v| v[:banner_id]}
+        
+        #get a subet for only current campaign id
         count = 0;
-        hashed_data2 = Array.new
-        for campaign in hashed_data
-            if campaign[:campaign_id].to_i == campaign_id.to_i
-        #        hello_test += campaign[:campaign_id].to_s + " " + campaign[:click_id].to_s + " "+ campaign[:banner_id].to_s + "\n"
-                hashed_data2[count] = campaign.to_h
+        clicks_data_subset = Array.new
+        for click in clicks_data
+            if click[:campaign_id].to_i == campaign_id.to_i
+                clicks_data_subset[count] = click.to_h
                 count += 1
             end
         end
-        hello_test += "---" + hashed_data2[5][:click_id].to_s + "\n"        
-        hashed_data2.sort_by! {|v| v[:banner_id]}
-#        hashed_data2.reverse!
-        hello_test += hashed_data2.to_s 
+        clicks_data_subset.sort_by! {|v| v[:banner_id]}
+        #hello_test += clicks_data_subset.to_s 
+
+        banner_revenue = Array.new
+        count = 0
+        for click in clicks_data_subset
+            hello_test += click[:click_id].to_s + " "
+            for conversion in conversions_data
+                if (click[:click_id].to_i == conversion[:click_id].to_i)
+                    hello_test += "click generated " + conversion[:revenue].to_s + " on banner " + click[:banner_id].to_s
+                    banner_revenue[count] = {:banner_id => click[:banner_id], :revenue => conversion[:revenue]}
+                    count += 1
+                end
+            end  
+            hello_test += "\n"
+        end
+
+        hello_test += banner_revenue.to_s
+
+           
 
         return hello_test
     end
