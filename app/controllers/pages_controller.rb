28763a6f74a42805fe3631d9e8c2bf0ade4aef7f
Philip Necsulescu <necsup@gmail.com>
2015-07-24 06:39:30 +0200

    check if campaign_id >= 5 works in controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
 
     def campaigns
-#     if @campaign_id >= 5
-#            csv_text = File.read('blah')
- #       end
+        if params[:campaign_id].to_i >= 5
+            csv_text = File.read('blah')
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
     get '/campaigns/:campaign_id', to: 'pages#campaigns', as: 'campaign_id' 
 
-#    get '/campaigns/:id', to: 'pages#campaigns'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
