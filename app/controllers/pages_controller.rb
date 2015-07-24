ba9d40f3287354033d6a3008d1dcce367f777877
Philip Necsulescu <necsup@gmail.com>
2015-07-24 17:12:58 +0200

    Banners revenues are summed up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -34,26 +34,36 @@ class PagesController < ApplicationController
                 count += 1
             end
         end
+
+        #banners with conversions, note that the clicks have to be sorted according to banner_id for this block
+        #since it sums up the revenues for each banner and checks if last_banner_id == banner_id
+
         clicks_data_subset.sort_by! {|v| v[:banner_id]}
-        #hello_test += clicks_data_subset.to_s 
 
         banner_revenue = Array.new
         count = 0
+        last_banner_id = -1
         for click in clicks_data_subset
-            hello_test += click[:click_id].to_s + " "
+#            hello_test += click[:click_id].to_s + " "
             for conversion in conversions_data
                 if (click[:click_id].to_i == conversion[:click_id].to_i)
                     hello_test += "click generated " + conversion[:revenue].to_s + " on banner " + click[:banner_id].to_s
-                    banner_revenue[count] = {:banner_id => click[:banner_id], :revenue => conversion[:revenue]}
-                    count += 1
+   
+                            banner_revenue[count] = {:banner_id => click[:banner_id], :revenue => conversion[:revenue]}
+                            if last_banner_id == click[:banner_id]
+                                banner_revenue[count-1][:revenue]  += conversion[:revenue]
+                            else
+                                last_banner_id = click[:banner_id]
+                                count += 1
+                            end
                 end
             end  
             hello_test += "\n"
         end
 
-        hello_test += banner_revenue.to_s
 
-           
+        hello_test += banner_revenue.to_s
+   
 
         return hello_test
     end
