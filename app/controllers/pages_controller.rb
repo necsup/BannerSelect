cf051df621db6ab01b21ac9733cc10eaac331870
Philip Necsulescu <necsup@gmail.com>
2015-07-24 11:59:13 +0200

    Display clicks.csv in view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,25 @@
 class PagesController < ApplicationController
-require 'csv'
 
     def campaigns
         if params[:campaign_id].to_i >= 5
-            csv_clicks= File.read("app/assets/csv/clicks_1.csv")
-            csv_convs = File.read("app/assets/csv/conversions_1.csv")
-            csv_imps = File.read("app/assets/csv/impressions_1.csv")
-
-            csv_clicks = CSV.parse(csv_clicks, :headers => true)
-            csv_convs =  CSV.parse(csv_convs, :headers => true)
-            csv_imps = CSV.parse(csv_imps, :headers => true)
         end
     end
 
     def say_hello(name)
         hello_test = "hello, " + name
         hello_test += " welcome"
+    
+            csv_clicks= File.read("app/assets/csv/clicks_1.csv")
+            csv_convs = File.read("app/assets/csv/conversions_1.csv")
+            csv_imps = File.read("app/assets/csv/impressions_1.csv")
+
+            csv_clicks = CSV.parse(csv_clicks, :headers => true) do |row|
+                hello_test += row.to_s
+            end
+
+            csv_convs =  CSV.parse(csv_convs, :headers => true)
+            csv_imps = CSV.parse(csv_imps, :headers => true)
+#       hello_test += csv_clicks
         return hello_test
     end
 end
